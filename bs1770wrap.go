28bd0e80b062d2f80f01295f9151d050c4c79660
Burillo <11786086+burillo-se@users.noreply.github.com>
2019-01-27 18:46:52 +0000

    Add support for sox
    Signed-off-by: Burillo <11786086+burillo-se@users.noreply.github.com>

--- bs1770wrap.go
+++ bs1770wrap.go
@@ -5,14 +5,18 @@ import (
 	"encoding/xml"
 	"fmt"
 	"os/exec"
+	"regexp"
+	"strconv"
 )
 
 // LoudnessData struct used to return result of
-// running bs1770 and calculating gain
+// running bs1770gain and calculating gain, as
+// well as running sox and calculating length
 type LoudnessData struct {
-	IntegratedLoudness float32
-	TruePeak           float32
-	LoudnessRange      float32
+	IntegratedLoudness float32 // lufs
+	TruePeak           float32 // lufs
+	LoudnessRange      float32 // lufs
+	Length             float32 // seconds
 }
 
 /* Data format:
@@ -72,17 +76,54 @@ type bs1770gainData struct {
 // analyze it with bs1770gain, and return a struct populated
 // with data we're interested in.
 func CalculateLoudness(file string) (LoudnessData, error) {
-	cmd := exec.Command("bs1770gain",
+	var out bytes.Buffer
+
+	sampleRegex, err := regexp.Compile(`Length \(seconds\):\s+(?P<len>\d+(\.\d+)?)`)
+	if err != nil {
+		return LoudnessData{}, fmt.Errorf("Cannot compile regex: %v", err)
+	}
+
+	cmd := exec.Command("sox",
+		file, // file to scan
+		"-n", // gather statistics
+		"stat",
+	)
+	cmd.Stderr = &out
+
+	err = cmd.Run()
+	if err != nil {
+		return LoudnessData{}, fmt.Errorf("Cannot get audio length: %v", err)
+	}
+
+	// get length from regex
+	matches := sampleRegex.FindStringSubmatch(out.String())
+
+	result := make(map[string]string)
+	for i, name := range matches {
+		result[sampleRegex.SubexpNames()[i]] = name
+	}
+	lenstr, ok := result["len"]
+	if !ok {
+		return LoudnessData{}, fmt.Errorf("Cannot get audio length: regex did not match")
+	}
+
+	len64, err := strconv.ParseFloat(lenstr, 32)
+	if err != nil {
+		return LoudnessData{}, fmt.Errorf("Cannot parse audio length: %v", err)
+	}
+
+	out.Reset()
+
+	cmd = exec.Command("bs1770gain",
 		"-itr",             // integrated, true peak, range
 		"--loglevel=quiet", // remove all non-essential output
 		"--xml",            // get XML output
 		file,               // what file to scan
 	)
 
-	var out bytes.Buffer
 	cmd.Stdout = &out
 
-	err := cmd.Run()
+	err = cmd.Run()
 	if err != nil {
 		return LoudnessData{}, fmt.Errorf("Cannot calculate loudness: %v", err)
 	}
@@ -97,6 +138,7 @@ func CalculateLoudness(file string) (LoudnessData, error) {
 	ld.IntegratedLoudness = gd.Album.Track.IntegratedLoudness.Value
 	ld.LoudnessRange = gd.Album.Track.LoudnessRange.Value
 	ld.TruePeak = gd.Album.Track.TruePeak.Value
+	ld.Length = float32(len64)
 
 	return ld, nil
 }
